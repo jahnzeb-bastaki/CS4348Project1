83ed9393dc9143d5cc8cb2772bd64594b11dac38
Jahnzeb Bastaki <jahnzebbastaki@gmail.com>
2024-02-22 15:09:59 -0600

    Added cpu/mem pipes and moved fopen command in child

--- project.c
+++ project.c
@@ -4,9 +4,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <time.h>
+
+int to_cpu[2], to_memory[2];
 
 //Helper Functions
-int loadData(char *dataFile, int memory[]){
+int loadData(FILE *dataFile, int memory[]){
   int array_p = 0;
   while(!feof(dataFile)){
     char fileline[50];
@@ -27,12 +30,26 @@ int loadData(char *dataFile, int memory[]){
 
 //CPU
 void parent(){
-  int PC, SP, IR, AC, X, Y;
+  srand(time(0));
+  int pc, sp, ir, ac, x, y;
+
+  
 }
 
-void child(char *filename){
+void child(char *argv){
+  FILE *file = fopen(argv, "r");
+  // File not found
+  if(!file){
+    fprintf(stderr, "ERROR - file \"%s\" was not found", argv);
+    exit(1);
+  }
+
   int memory[2000];
-  int len = loadData(filename, memory);
+  int len = loadData(file, memory);
+
+  int usr_program = 0;
+  int sys_program = 1000;
+
   
 
 }
@@ -45,14 +62,9 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "ERROR - Usage: %s <filename> <interrupt>\n", argv[0]);
     exit(1);
   }
-  FILE *file = fopen(argv[1], "r");
-  // File not found
-  if(!file){
-    fprintf(stderr, "ERROR - file \"%s\" was not found", argv[1]);
-    exit(1);
-  }
-  child(file);
+  
+  pipe(to_cpu); pipe(to_memory);
 
-  fclose(file);
+  fork() ? parent() : child(argv[1]); 
 }
 
